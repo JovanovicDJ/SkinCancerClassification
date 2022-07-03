ee5371903e2d341a80b6c3dbe59dc97809ee6593
Djordje Jovanovic <djordjejovanovic27@gmail.com>
2022-07-03 15:41:42 +0200

    Formed model and training

--- SkinCancerClassification/model_two.py
+++ SkinCancerClassification/model_two.py
@@ -17,14 +17,19 @@ from keras.callbacks import ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
-
 if __name__ == '__main__':
     warnings.simplefilter(action="ignore", category=FutureWarning)
+    warnings.simplefilter(action="ignore", category=UserWarning)
+    warnings.simplefilter(action="ignore", category=RuntimeWarning)
+    warnings.simplefilter(action='ignore', category=DeprecationWarning)
+    warnings.filterwarnings('ignore')
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
     # Reading already prepared data, images are loaded as 28x28x3 RGB arrays and they are labeled
     data = pd.read_csv('HAM10000/hmnist_28_28_RGB.csv')
 
     # Balancing dataset for more precision in results
+    print('Preparing data...')
     data = data.sort_values('label')
     data = data.reset_index()
 
@@ -57,6 +62,12 @@ if __name__ == '__main__':
     new_data = final_data.iloc[:, :-1]
     labels = final_data.iloc[:, -1:]
 
+    type_cancer = ['nv', 'mel', 'bkl', 'bcc', 'akiec', 'vasc', 'df']
+    counts = list(labels.value_counts())
+    plt.figure(figsize=(8, 6))
+    sns.barplot(x=type_cancer, y=counts)
+    plt.show()
+
     # Preparing data for neural network (data split for training and testing)
     X = np.array(new_data)
     Y = np.array(labels)
@@ -79,3 +90,91 @@ if __name__ == '__main__':
 
     train_data = train_datagen.flow(X_train, Y_train, batch_size=64)
     test_data = test_datagen.flow(X_test, Y_test, batch_size=64)
+
+    # Model
+    model = Sequential()
+    model.add(Conv2D(16, kernel_size=(3, 3), input_shape=(28, 28, 3), activation='relu', padding='same'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Flatten())
+
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+
+    learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
+                                                patience=3,
+                                                verbose=1)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+
+    print(model.summary())
+
+    # Training
+    history = model.fit(X_train,
+                        Y_train,
+                        validation_split=0.2,
+                        batch_size=32,
+                        epochs=10,
+                        callbacks=[learning_rate_reduction])
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    plt.figure(figsize=(8, 6))
+    plt.title("Training and Validation accuracy of the model")
+    plt.plot(acc, label='train_acc')
+    plt.plot(val_acc, label='val_acc')
+    plt.legend()
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.figure(figsize=(8, 6))
+    plt.title("Training and Validation loss of the model")
+    plt.plot(loss, label='train_loss')
+    plt.plot(val_loss, label='val_loss')
+    plt.legend()
+    plt.show()
+
+    Y_true = np.array(Y_test)
+
+    Y_pred = model.predict(X_test)
+    Y_pred = np.array(list(map(lambda x: np.argmax(x), Y_pred)))
+
+    cm1 = confusion_matrix(Y_true, Y_pred)
+    plt.figure(figsize=(12, 6))
+    plt.title('The confusion matrix of model on test set')
+    sns.heatmap(cm1, annot=True, fmt='g', vmin=0, cmap='viridis')
+    plt.show()
+
+    label_mapping = {
+        0: 'nv',
+        1: 'mel',
+        2: 'bkl',
+        3: 'bcc',
+        4: 'akiec',
+        5: 'vasc',
+        6: 'df'
+    }
+
+    classification_report_model = classification_report(Y_true, Y_pred, target_names=label_mapping.values())
+    print(classification_report_model)
+
+    model_acc_test = model.evaluate(X_test, Y_test, verbose=0)[1]
+    print("Test Accuracy of model: {:.3f}%".format(model_acc_test * 100))
